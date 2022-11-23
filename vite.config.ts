e1f5107b048e75e0a73d5759008815f80cb92603
mistadave <std@lemt.ch>
2022-11-23 18:25:56 +0100

    add vite-plugin-dts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   plugins: [
+    dts(),
     vue({
       reactivityTransform: true
     })
