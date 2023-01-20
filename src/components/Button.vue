ba153bf48175833f4963da9880b942aaa092b439
Bahadır Sofuoğlu <bahadirsofuoglu@gmail.com>
2023-01-21 00:52:17 +0300

    Slot updates #28

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,29 +1,29 @@
 <template>
-  <button :class="getButtonClass" :disabled="options.disabled">
-    <span v-if="!options.hideText">{{ options.text }}</span>
-    <i :class="getIconClass(options.icon)" />
-  </button>
+    <button :class="getButtonClass" :disabled="options.disabled">
+        <span v-if="!options.hideText">{{ options.text }}</span>
+        <i :class="getIconClass(options.icon)" />
+    </button>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
 const props = defineProps({
-  options: {
-    type: Object,
-    default: function () {
-      return {}
+    options: {
+        type: Object,
+        default: function () {
+            return {}
+        }
     }
-  }
 })
 
 const getIconClass = (iconName: string) => `pi pi-${iconName}`
 
 const getButtonClass = computed(() => {
-  return [
-    'fw-btn',
-    {
-      'fw-btn-disabled': props.options.disabled
-    }
-  ]
+    return [
+        'fw-btn',
+        {
+            'fw-btn-disabled': props.options.disabled
+        }
+    ]
 })
 </script>
--- src/components/WizardStep.vue
+++ src/components/WizardStep.vue
@@ -1,68 +1,67 @@
 <template>
-  <li>
-    <div
-      v-if="showProgress"
-      class="fw-list-progress"
-      :class="{ 'fw-list-progress-active': progressActive }"
-    ></div>
-    <div class="fw-list-wrapper">
-      <div
-        :id="`step-${tab.id}`"
-        class="fw-list-wrapper-icon"
-        role="tab"
-        :class="[
-          {
-            'fw-step-active': tab.active,
-            'fw-step-checked': tab.checked,
-            'fw-squared-tab': squaredTab
-          }
-        ]"
-      >
-        <div class="fw-step-container">
-          <slot name="active-step">
-            <i v-if="props.tab.icon" :class="iconClass"></i>
-            <i v-else>{{ index + 1 }}</i>
-          </slot>
+    <li>
+        <div
+            v-if="showProgress"
+            class="fw-list-progress"
+            :class="{ 'fw-list-progress-active': progressActive }"
+        ></div>
+        <div class="fw-list-wrapper">
+            <div :id="`step-${tab.id}`" role="tab" :class="containerClasses">
+                <div class="fw-step-container">
+                    <slot name="active-step">
+                        <i v-if="props.tab.icon" :class="iconClass"></i>
+                        <i v-else>{{ index + 1 }}</i>
+                    </slot>
+                </div>
+            </div>
+            <slot name="title">
+                <span
+                    class="fw-step-title"
+                    :class="{
+                        active: tab.active
+                    }"
+                >
+                    {{ tab.title }}
+                </span>
+            </slot>
         </div>
-      </div>
-      <slot name="title">
-        <span
-          class="fw-step-title"
-          :class="{
-            active: tab.active
-          }"
-        >
-          {{ tab.title }}
-        </span>
-      </slot>
-    </div>
-  </li>
+    </li>
 </template>
 <script setup lang="ts">
 import { computed } from 'vue'
 
 const props = defineProps({
-  tab: {
-    type: Object,
-    default: () => {}
-  },
-  index: {
-    type: Number,
-    default: 0
-  },
-  currentIndex: {
-    type: Number,
-    default: 0
-  },
-  squaredTab: {
-    type: Boolean,
-    default: false
-  },
-  showProgress: {
-    type: Boolean,
-    default: true
-  }
+    tab: {
+        type: Object,
+        default: () => {}
+    },
+    index: {
+        type: Number,
+        default: 0
+    },
+    currentIndex: {
+        type: Number,
+        default: 0
+    },
+    squaredTab: {
+        type: Boolean,
+        default: false
+    },
+    showProgress: {
+        type: Boolean,
+        default: true
+    }
 })
 const progressActive = computed(() => props.currentIndex > props.index)
 const iconClass = computed(() => `pi pi-${props.tab.icon}`)
+const containerClasses = computed(() => {
+    return [
+        'fw-list-wrapper-icon',
+        {
+            'fw-step-active': props.tab.active,
+            'fw-step-checked': props.tab.checked,
+            'fw-squared-tab': props.squaredTab
+        }
+    ]
+})
 </script>
